eda21dbe32893cc4f056f3673e302e550d47d520
Shubham Das <das005955@gmail.com>
2025-09-17 11:56:35 +0000

    let the ui support on desktop browser also

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -3,20 +3,20 @@ import * as React from "react"
 const MOBILE_BREAKPOINT = 768
 
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
+  const [isMobile, setIsMobile] = React.useState(false)
 
   React.useEffect(() => {
     const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
     const onChange = () => {
       setIsMobile(mql.matches)
     }
+
     mql.addEventListener("change", onChange)
-    
     // Set the initial state
     onChange();
 
     return () => mql.removeEventListener("change", onChange)
   }, [])
 
-  return !!isMobile
+  return isMobile
 }
